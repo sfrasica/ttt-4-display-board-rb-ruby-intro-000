a1021c2ae456e3f7520b041036ca855d6a6aad07
Steven Frasica <sfrasica1@gmail.com>
2019-11-22 20:55:05 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,5 +7,5 @@ def display_board(board)
     puts "-----------"
     puts "   |   |   "
 end
-board = [" O ", " ", " ", " ", " ", " ", " ", " ", " "]
+board = [" O ", " ", " ", " ", " X ", " ", " ", " ", " "]
 display_board(board)
